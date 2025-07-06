1dbe0dbef4b780f0f13a97956bde033490a3c4ac
yatin Gaur <yatingaur66@gmail.com>
2025-07-06 10:40:49 +0530

    Refactor first.py to include variable examples and maintain consistent formatting

--- first.py
+++ first.py
@@ -1,3 +1,12 @@
 print("print(""wha"")")
 print("Namaste bharat")
 print("hello"+"world")
+
+#varibles
+
+yatinGaur="yatin"#camelcase
+yatin_gaur="yatin"#small snake case
+YatinGaur="yatin"#pascal case
+Yatin_Gaur="yatin"#capital snake case
+
+print(len(yatin_gaur))
