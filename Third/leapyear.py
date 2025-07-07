ca7994650a57ebca351b08f481edad07a19685c1
yatin Gaur <yatingaur66@gmail.com>
2025-07-07 18:49:59 +0530

    Add leap year calculation script

---
+++ Third/leapyear.py
@@ -0,0 +1,11 @@
+year=int(input("Enter the year"))
+if (year%100==0 and year%400==0):
+    {
+       print("leap year")
+    }
+elif (year%100!=0 and year%4==0):
+    {
+        print("leap year")
+    }
+else:
+    print("not leap year")    
